27066863eb2605eb2793709060b1bdd9e1f15d79
Mr-Quin <hr82003@gmail.com>
2021-02-25 00:04:08 -0800

    Update Api.ts

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,31 +1,37 @@
 import { UrlOption } from '../client/getUrl'
+import { ApiAcceptedReturns, ApiKeyMaster, Arrayable, Data } from './index'
 
 type RequestMethod = 'get' | 'post'
 
 interface ApiConfig {
     method: RequestMethod
-    require?: Array<string | string[]>
-    optional?: Array<string | string[]>
-    parents: string[]
-    callback: (payload: any, options?: Partial<UrlOption>) => any
+    require?: Arrayable<Arrayable<string>>
+    optional?: Arrayable<Arrayable<string>>
+    parents?: ApiKeyMaster[]
+    headers?: Partial<UrlOption['headers']>
+    action: (payload: any, options?: Partial<UrlOption>) => any
 }
 
-export default class Api<T> {
-    readonly name: string
+export default class Api<T extends ApiAcceptedReturns> {
+    readonly name: ApiKeyMaster
+    readonly method: ApiConfig['method']
     readonly require: ApiConfig['require']
     readonly optional: ApiConfig['optional']
     readonly parents: ApiConfig['parents']
-    private readonly callback: ApiConfig['callback']
+    readonly headers: ApiConfig['headers']
+    private readonly callback: ApiConfig['action']
 
-    constructor(name: string, config: ApiConfig) {
+    constructor(name: ApiKeyMaster, config: ApiConfig) {
         this.name = name
-        this.require = config.require
-        this.optional = config.optional
-        this.parents = config.parents
-        this.callback = config.callback
+        this.method = config.method ?? 'get'
+        this.require = config.require ?? []
+        this.optional = config.optional ?? []
+        this.parents = config.parents ?? []
+        this.headers = config.headers ?? {}
+        this.callback = config.action
     }
 
-    get = (option?: Partial<UrlOption>) => async (payload: any): Promise<T> => {
+    get = (option?: Partial<UrlOption>) => async (payload: Data): Promise<T> => {
         return this.callback(payload, option)
     }
 }
