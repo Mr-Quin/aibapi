97d9eb881da4c2ab97a2401001b51b2722801cda
Mr-Quin <hr82003@gmail.com>
2021-02-25 00:04:19 -0800

    Update video.ts

--- src/api/video.ts
+++ src/api/video.ts
@@ -2,19 +2,25 @@ import getUrl, { UrlOption } from '../client/getUrl'
 import omit from 'lodash/omit'
 import { mapSeries } from 'async'
 import Api from './Api'
-import biliStore from '../store/store'
-import * as Bilibili from '../types'
+import biliStore from '../store'
+import {
+    CoinResponse,
+    GeneralResponse,
+    TripleResponse,
+    VideoInfoResponse,
+    VideoStreamResponse,
+} from './index'
+import { isSignedIn } from '../index'
 
 export default {
-    videoInfo: new Api<Bilibili.VideoInfoResponse>('videoInfo', {
+    videoInfo: new Api<VideoInfoResponse>('videoInfo', {
         method: 'get',
         require: [['aid', 'bvid']],
-        optional: [],
         parents: [],
-        callback: async (payload, options) => {
+        action: (payload, options) => {
             const defaultHeaders: Partial<UrlOption['headers']> = {
-                origin: 'https://www.bilibili.com',
-                host: 'api.bilibili.com',
+                Origin: 'https://www.bilibili.com',
+                Host: 'api.bilibili.com',
             }
             const headers = { ...options?.headers, ...defaultHeaders }
             return getUrl('https://api.bilibili.com/x/web-interface/view', {
@@ -23,18 +29,17 @@ export default {
             })(payload)
         },
     }),
-    videoStream: new Api<Bilibili.VideoStreamResponse[]>('videoStream', {
+    videoStream: new Api<VideoStreamResponse[]>('videoStream', {
         method: 'get',
         require: [['avid', 'bvid']],
         optional: ['type', 'platform', 'high_quality', 'qn', 'fourk', 'fnval'],
         parents: ['cid'],
-        callback: async (payload, options) => {
-            const defaultHeaders: Partial<UrlOption['headers']> = {
-                origin: 'https://www.bilibili.com',
-                host: 'api.bilibili.com',
-            }
-            const headers = { ...options?.headers, ...defaultHeaders }
-            const defaultPayload = biliStore.getState().SESSDATA
+        headers: {
+            Origin: 'https://www.bilibili.com',
+            Host: 'api.bilibili.com',
+        },
+        action: async (payload, options) => {
+            const defaultPayload = isSignedIn()
                 ? { qn: payload.qn ?? 120, fourk: 1, fnval: 0 }
                 : {
                       type: 'mp4',
@@ -43,10 +48,10 @@ export default {
                   }
             if (!Array.isArray(payload.cid)) {
                 return [
-                    await getUrl('https://api.bilibili.com/x/player/playurl', {
-                        ...options,
-                        headers,
-                    })({
+                    await getUrl(
+                        'https://api.bilibili.com/x/player/playurl',
+                        options
+                    )({
                         ...defaultPayload,
                         ...payload,
                     }),
@@ -64,49 +69,80 @@ export default {
             }
         },
     }),
-    like: new Api<string>('like', {
+    like: new Api<GeneralResponse>('like', {
         method: 'post',
         require: [['aid', 'bvid']],
-        optional: [],
-        parents: [],
-        callback: async (payload, options) => {
-            const defaultHeaders: Partial<UrlOption['headers']> = {
-                origin: 'https://www.bilibili.com',
-                host: 'api.bilibili.com',
-            }
-            const headers = { ...options?.headers, ...defaultHeaders }
+        optional: ['like'],
+        headers: {
+            Origin: 'https://www.bilibili.com',
+            Host: 'api.bilibili.com',
+        },
+        action: async (payload, options) => {
             const defaultPayload = {
                 like: 1,
                 csrf: biliStore.getState().bili_jct,
             }
-            return getUrl('http://api.bilibili.com/x/web-interface/archive/like', {
-                ...options,
-                headers,
-            })({ ...payload, ...defaultPayload })
+            return getUrl(
+                'http://api.bilibili.com/x/web-interface/archive/like',
+                options
+            )({ ...defaultPayload, ...payload })
+        },
+    }),
+    coin: new Api<CoinResponse>('coin', {
+        method: 'post',
+        require: [['aid', 'bvid']],
+        optional: ['multiply', 'like'],
+        headers: {
+            Origin: 'https://www.bilibili.com',
+            Host: 'api.bilibili.com',
+        },
+        action: async (payload, options) => {
+            const defaultPayload = {
+                multiply: 1,
+                csrf: biliStore.getState().bili_jct,
+            }
+            return getUrl(
+                'http://api.bilibili.com/x/web-interface/coin/add',
+                options
+            )({ ...defaultPayload, ...payload })
+        },
+    }),
+    triple: new Api<TripleResponse>('triple', {
+        method: 'post',
+        require: [['aid', 'bvid']],
+        headers: {
+            Origin: 'https://www.bilibili.com',
+            Host: 'api.bilibili.com',
+        },
+        action: async (payload, options) => {
+            const defaultPayload = {
+                csrf: biliStore.getState().bili_jct,
+            }
+            return getUrl(
+                'http://api.bilibili.com/x/web-interface/archive/like/triple',
+                options
+            )({ ...defaultPayload, ...payload })
         },
     }),
     videoTitle: new Api<string>('videoTitle', {
         method: 'get',
         parents: ['videoInfo'],
-        callback: (payload: { videoInfo: Bilibili.VideoInfoResponse }) =>
-            payload.videoInfo.data!.title,
+        action: (payload: { videoInfo: VideoInfoResponse }) => payload.videoInfo.data?.title,
     }),
     aid: new Api<number>('aid', {
         method: 'get',
         parents: ['videoInfo'],
-        callback: (payload: { videoInfo: Bilibili.VideoInfoResponse }) =>
-            payload.videoInfo.data!.aid,
+        action: (payload: { videoInfo: VideoInfoResponse }) => payload.videoInfo.data?.aid,
     }),
     bvid: new Api<number>('bvid', {
         method: 'get',
         parents: ['videoInfo'],
-        callback: (payload: { videoInfo: Bilibili.VideoInfoResponse }) =>
-            payload.videoInfo.data!.bvid,
+        action: (payload: { videoInfo: VideoInfoResponse }) => payload.videoInfo.data?.bvid,
     }),
     cid: new Api<number[]>('cid', {
         method: 'get',
         parents: ['videoInfo'],
-        callback: (payload: { videoInfo: Bilibili.VideoInfoResponse }) =>
-            payload.videoInfo.data!.pages.map((page) => page.cid),
+        action: (payload: { videoInfo: VideoInfoResponse }) =>
+            payload.videoInfo.data?.pages.map((page) => page.cid),
     }),
 }
