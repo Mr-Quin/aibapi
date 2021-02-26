71827f21ff10ca6b22526b2f6c6150fcd7e9de0f
Mr-Quin <hr82003@gmail.com>
2021-02-25 16:35:33 -0800

    Update test.ts

--- tests/api/test.ts
+++ tests/api/test.ts
@@ -2,6 +2,7 @@ import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import { biliRequest, isSignedIn, biliConfig } from '../../src'
 import { config as env } from 'dotenv-flow'
+import getUrl from '../../src/client/getUrl'
 
 chai.use(chaiAsPromised)
 const { expect } = chai
@@ -16,7 +17,7 @@ const videoBvid = {
     vid: 'BV1ax411c71N', // 放学后的Pleiades 四夜全【TD】
 }
 const videoAid = {
-    vid: '1216974', // 【岚少实况】 水果探求者  【完结】【任性女神俏天使】
+    vid: 2233, //[初音ミクと 巡音ルカと メグポイド] 千年の雪
 }
 const videoInvalid = {
     vid: 0,
@@ -33,16 +34,13 @@ biliConfig({ SESSDATA: SESSDATA, bili_jct: bili_jct, debug: true })
 
 describe('Basic tests', () => {
     it('can sign in', async () => {
-        const result = await biliRequest((api) => api.myInfo)
+        const info = await biliRequest((api) => api.videoInfo)
+
         expect(isSignedIn()).to.equal(true)
-        expect(result)
-            .to.be.an('object')
-            .to.have.property('myInfo')
-            .to.have.property('code')
-            .to.equal(0)
+        expect(info).to.have.property('code').to.equal(0)
     })
     it('can access membership api', async () => {
-        const { memberInfo, memberName } = await biliRequest(
+        const [memberInfo, memberName] = await biliRequest(
             (api) => [api.memberInfo, api.memberName],
             member
         )
@@ -50,7 +48,7 @@ describe('Basic tests', () => {
         expect(memberName).to.be.an('string').to.equal('saber酱')
     })
     it('can access video api', async () => {
-        const { videoInfo, videoTitle } = await biliRequest(
+        const [videoInfo, videoTitle] = await biliRequest(
             (api) => [api.videoInfo, api.videoTitle],
             videoBvid
         )
@@ -58,7 +56,7 @@ describe('Basic tests', () => {
         expect(videoTitle).to.be.an('string').to.equal('放学后的Pleiades 四夜全【TD】')
     })
     it('can chain api', async () => {
-        const { videoStream } = await biliRequest(({ videoStream }) => videoStream, videoAid)
+        const videoStream = await biliRequest(({ videoStream }) => videoStream, videoAid)
         expect(videoStream).to.be.an('array')
         videoStream.forEach((e) => {
             expect(e).to.have.property('code').to.equal(0)
@@ -72,32 +70,42 @@ describe('All api test', () => {
             ...videoInvalid,
             ...memberInvalid,
         })
-        console.log(JSON.stringify(all))
+        expect(all).to.be.an('array')
     }).timeout(50000)
 })
 
 describe('Interaction tests', () => {
     it('can like videos', async () => {
-        const { like } = await biliRequest(({ like }) => [like], videoBvid)
-        expect(like).to.be.an('object').to.have.property('code').to.be.an('number')
+        const like = await biliRequest(({ like }) => like, videoBvid)
+        expect(like).to.have.property('code').to.be.an('number')
     })
     it('can coin videos', async () => {
-        const { coin } = await biliRequest(({ coin }) => [coin], { ...videoBvid })
-        expect(coin).to.be.an('object').to.have.property('code').to.be.an('number')
+        const coin = await biliRequest(({ coin }) => coin, videoBvid)
+        expect(coin).to.have.property('code').to.be.an('number')
     })
     it('can triple combo', async () => {
-        const { triple } = await biliRequest(({ triple }) => [triple], { ...videoBvid })
-        expect(triple).to.be.an('object').to.have.property('code').to.be.an('number')
+        const triple = await biliRequest(({ triple }) => triple, videoBvid)
+        expect(triple).to.have.property('code').to.be.an('number')
     })
 })
 
 describe('Error tests', () => {
     it('rejects missing parameters', async () => {
-        expect(biliRequest(({ videoInfo }) => videoInfo)).to.be.rejectedWith(Error)
+        // const a = await biliRequest(({ videoInfo }) => videoInfo)
+        await expect(biliRequest(({ videoInfo }) => videoInfo)).to.eventually.be.rejectedWith(
+            'Missing parameters. Expected aid,bvid, got {}'
+        )
+    })
+    it('rejects anything other than 200 response code', async () => {
+        await expect(
+            getUrl('http://api.bilibili.com/x/web-interface/view/v')()
+        ).to.eventually.be.rejectedWith('Response code 404 (Not Found)')
     })
     it('rejects undefined api', async () => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        expect(biliRequest((api) => api.non)).to.be.rejectedWith(Error)
+        await expect(biliRequest((api) => api.non)).to.eventually.be.rejectedWith(
+            'Invalid api, api is undefined'
+        )
     })
 })
