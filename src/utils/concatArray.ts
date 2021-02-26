453bbdfe01a0ce615d10d4064ebec430e1a3a4b1
Mr-Quin <hr82003@gmail.com>
2021-02-25 16:35:31 -0800

    Update concatArray.ts

--- src/utils/concatArray.ts
+++ src/utils/concatArray.ts
@@ -1,7 +1,7 @@
 // concat, but treats undefined as empty arrays
 const concatArray = <T>(...args: Array<any>): T[] => {
     return args.reduce<T[]>((acc, cur) => {
-        if (cur === undefined) return acc
+        if (cur === undefined || cur === null) return acc
         if (!Array.isArray(cur)) return [...acc, cur]
         return [...acc, ...cur]
     }, [])
