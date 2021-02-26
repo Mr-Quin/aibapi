48844d69440869ae623fb35a4c85c651185b2556
Mr-Quin <hr82003@gmail.com>
2021-02-25 16:35:22 -0800

    Update biliRequest.ts

--- src/client/biliRequest.ts
+++ src/client/biliRequest.ts
@@ -1,27 +1,41 @@
 import biliStore, { BiliCredential, BiliOptions } from '../store'
 import { parseParamsVid } from '../helper'
-import { reduce } from 'async'
+import { mapSeries } from 'async'
 import logger from '../service/logger'
 import { getApiRecursive } from './getApi'
-import bilibiliApi, { ApiList, ApiReturns, ApiValues, Data } from '../api'
+import bilibiliApi, { ApiList, ApiValue, ApiValues, Data, ExtractGeneric } from '../api'
 
 export const biliConfig = (options: Partial<BiliCredential & BiliOptions>) => {
     biliStore.setState({ ...options })
 }
 
-// not used for now
+export type RequestAcceptedInput = ApiValue[] | [ApiValue] | ApiValue
 export type ApiSelector<T> = (apiList: ApiList) => T
+export type RequestReturn<T extends ApiValues> = T extends ApiValue
+    ? ExtractGeneric<T>
+    : T extends [...ApiValue[]]
+    ? {
+          [I in keyof T]: ExtractGeneric<T[I]>
+      } & { length: T['length'] }
+    : never
 
-export const biliRequest = async <ApiSlice extends ApiValues>(
+export const biliRequest = async <ApiSlice extends RequestAcceptedInput>(
     selector: ApiSelector<ApiSlice>,
     params: Data = {}
-): Promise<ApiReturns> => {
+): Promise<RequestReturn<ApiSlice>> => {
     const cache = { ...parseParamsVid(params) }
-    const api: ApiValues = selector(bilibiliApi)
-    const targets = Array.isArray(api) ? api : [api]
-    return (reduce(targets, {}, async (acc, target) => {
-        const result = await getApiRecursive(target)(parseParamsVid(params), cache)
-        logger.debug(`Target "${JSON.stringify(target.name)}", result: ${JSON.stringify(result)}`)
-        return { ...acc, ...{ [target.name]: result } }
-    }) as unknown) as ApiReturns
+    const api: RequestAcceptedInput = selector(bilibiliApi)
+    if (Array.isArray(api)) {
+        return (await mapSeries(api, async (target) => {
+            const result = await getApiRecursive(target)(parseParamsVid(params), cache)
+            logger.debug(
+                `Target "${JSON.stringify(target.name)}", result: ${JSON.stringify(result)}`
+            )
+            return result
+        })) as RequestReturn<ApiSlice>
+    } else {
+        const result = await getApiRecursive(api)(parseParamsVid(params), cache)
+        logger.debug(`Target "${JSON.stringify(api.name)}", result: ${JSON.stringify(result)}`)
+        return result as RequestReturn<ApiSlice>
+    }
 }
