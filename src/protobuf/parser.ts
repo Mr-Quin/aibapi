430414a9b03187d3d8458c7d2690d69da99d2e0a
Mr-Quin <hr82003@gmail.com>
2021-02-27 00:21:34 -0800

    Update parser.ts

--- src/protobuf/parser.ts
+++ src/protobuf/parser.ts
@@ -1,25 +1,15 @@
 import protobuf from 'protobufjs'
 import { bilibili } from './dm'
 
-export const decode = async (buffer: any) => {
-    const root = await protobuf.load('./src/protobuf/dm.proto')
-    const danmu = root.lookupType('bilibili.community.service.dm.v1.DanmakuElem')
-    const binary = Buffer.from(buffer, 'utf8')
-    const b = new protobuf.BufferReader(binary)
+export const decode = (buffer: Buffer) => {
+    // const root = await protobuf.load('./src/protobuf/dm.proto')
+    // const danmu = root.lookupType('bilibili.community.service.dm.v1.DanmakuElem')
 
-    const err = danmu.verify(b)
+    const err = bilibili.community.service.dm.v1.DmSegMobileReply.verify(buffer)
     if (err) {
-        console.log(err)
         throw err
     }
-    console.log(b, binary)
-    const m = bilibili.community.service.dm.v1.DmSegMobileReply.verify(b)
-    console.log(m)
-    const m2 = bilibili.community.service.dm.v1.DmSegMobileReply.decodeDelimited(b)
-    console.log(m2)
-    // console.log('DanmakuElem', m2)
-    // const message = danmu.decode(b)
-    // console.log(message)
-    // return danmu.toObject(message)
-    return 0
+    const decode = bilibili.community.service.dm.v1.DmSegMobileReply.decode(buffer)
+    console.log(decode.elems[0])
+    return decode
 }
